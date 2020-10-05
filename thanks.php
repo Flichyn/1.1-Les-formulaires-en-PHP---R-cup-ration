ff9a2d8cb9e2aedffc4747baf8a9dbb53eadb4f4
Flichyn <70525056+Flichyn@users.noreply.github.com>
2020-10-05 10:44:43 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -10,8 +10,10 @@
 <body>
 
 <p>
-    <?php echo 'Merci ' . $_POST['user_firstname'] . ' ' . $_POST['user_lastname'] . ' de nous avoir contacté à propos de ' . $_POST['user_subject'] .'.'; ?><br/>
-    <?php echo 'Un de nos conseillers vous contactera soit à l’adresse ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['user_phone'] . ' dans les plus brefs délais pour traiter votre demande :'; ?><br/>
+    <?php echo 'Merci ' . $_POST['user_firstname'] . ' ' . $_POST['user_lastname'] . ' de nous avoir contacté à propos de ' . $_POST['user_subject'] . '.'; ?>
+    <br/>
+    <?php echo 'Un de nos conseillers vous contactera soit à l’adresse ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['user_phone'] . ' dans les plus brefs délais pour traiter votre demande :'; ?>
+    <br/>
     <?php echo $_POST['user_message']; ?><br/>
 </p>
 
