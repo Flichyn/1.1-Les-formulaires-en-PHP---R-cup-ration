d072b9509873edac3f54c961333a12c147e3ca47
Flichyn <70525056+Flichyn@users.noreply.github.com>
2020-10-05 13:39:49 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -10,11 +10,18 @@
 <body>
 
 <p>
-    <?php echo 'Merci ' . $_POST['user_firstname'] . ' ' . $_POST['user_lastname'] . ' de nous avoir contacté à propos de ' . $_POST['user_subject'] . '.'; ?>
-    <br/>
-    <?php echo 'Un de nos conseillers vous contactera soit à l’adresse ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['user_phone'] . ' dans les plus brefs délais pour traiter votre demande :'; ?>
-    <br/>
-    <?php echo $_POST['user_message']; ?><br/>
+    <?php
+
+    if (filter_var($email = $_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
+        echo 'Merci ' . $_POST['user_firstname'] . ' ' . $_POST['user_lastname'] . ' de nous avoir contacté à propos de ' . $_POST['user_subject'] . '.'; ?>
+        <br/>
+        <?php echo 'Un de nos conseillers vous contactera soit à l’adresse ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['user_phone'] . ' dans les plus brefs délais pour traiter votre demande :'; ?>
+        <br/>
+        <?php echo $_POST['user_message'];
+    } else {
+        echo "L'adresse email '$email' est invalide.";
+    } ?>
+
 </p>
 
 </body>
